e1013a0007d50819289fbdd2a7e512c23f4b727a
Ringo <Ringo.Evan@gmail.com>
2019-07-20 10:05:17 -0400

    Initial Commit

---
+++ Knight_Fit_Beta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Knight_Fit_Beta
+//
+//  Created by Evan Ringo on 7/20/19.
+//  Copyright © 2019 Evan Ringo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
