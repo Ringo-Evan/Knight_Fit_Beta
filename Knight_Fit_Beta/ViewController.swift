24f579cf8056108d7a2ee3486f0beed3565e3e3f
Ringo <Ringo.Evan@gmail.com>
2019-07-20 10:27:41 -0400

    Set up layout

--- Knight_Fit_Beta/ViewController.swift
+++ Knight_Fit_Beta/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timePickerSelect: UIPickerView!
+    
+    @IBOutlet weak var sectionPickerSelect: UIPickerView!
+    
+    @IBAction func movementSelect(_ sender: Any) {
+    }
+    
+    @IBAction func musicSelect(_ sender: Any) {
+    }
+    
+    @IBAction func startOrEnd(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
