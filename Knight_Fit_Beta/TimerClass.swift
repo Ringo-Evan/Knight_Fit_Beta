43698d79f2ed26b929b497b0dc8d24a18d8572eb
Evan Ringo <eringo@dekaresearch.com>
2019-07-26 16:55:03 -0400

    Timer Created, Need to connect buttons and get working

--- Knight_Fit_Beta/TimerClass.swift
+++ Knight_Fit_Beta/TimerClass.swift
@@ -8,20 +8,34 @@
 
 import Foundation
 
-class Timer{
+class DisplayTimer{
     
     //Time to countdown should always be # min * num sec
     //Currently hard coded to 5 misn
     let startTimeMin :Int
     let startTimeSec :Int
+    var internalTimer : Timer
+    var secondsLeft :Int
     
-    init(){
-        startTimeSec = 0
-        startTimeMin = 60*5
+    convenience init(){
+        self.init(min: 5*60, sec: 0)
     }
     init(min: Int, sec : Int){
         startTimeMin = min
         startTimeSec = sec
+        secondsLeft = startTimeMin + startTimeSec
+        internalTimer = Timer()
+        
+    }
+    
+    func runTimer()
+    {
+        internalTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() -> Int {
+        secondsLeft -= 1
+        return secondsLeft
     }
     
 }
--- Knight_Fit_Beta/ViewController.swift
+++ Knight_Fit_Beta/ViewController.swift
@@ -9,30 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var displayTimer : DisplayTimer = DisplayTimer()
+
 
     @IBOutlet weak var timePickerSelect: UIPickerView!
     
     @IBOutlet weak var sectionPickerSelect: UIPickerView!
     
-    @IBAction func movementSelect(_ sender: Any) {
-    }
-    
-    @IBAction func musicSelect(_ sender: Any) {
-    }
-    
+   
     @IBAction func startOrEnd(_ sender: Any) {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
     }
 
     @IBAction func startOrEndPressed(_ sender: Any) {
+        displayTimer.runTimer()
+        
+        
     }
     
     @IBAction func selectMusicPressed(_ sender: Any) {
     }
     @IBAction func movementSelectPressed(_ sender: Any) {
     }
+    
 }
 
